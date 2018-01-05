ee3cfe1772ba020cba13bd2784f96ef92ac6b07f
Anto <anto@MacBook-Pro-de-Anto.local>
2018-01-05 21:59:44 +0100

    Using botkit

--- cryptobot.js
+++ cryptobot.js
@@ -1,6 +1,6 @@
-const SlackClient = require('@slack/client');
 const utils = require('./utils');
 const fetch = require('node-fetch');
+const Botkit = require('botkit');
 
 const defaultOptions = {
   triggerOnWords: ['?cm'],	
@@ -13,34 +13,22 @@ const onAuthenticated = (rtmStartData) => {
 	defaultOptions.logger.info(`Logged in as ${rtmStartData.self.name} (id: ${botId}) of team ${rtmStartData.team.name}`);
 };
 
-const cryptoBot = function(botToken, CoinList) { 
-	const opt = Object.assign({}, defaultOptions);
-	const rtm = new SlackClient.RtmClient(botToken, opt.rtmOptions);
-	const web = new SlackClient.WebClient(botToken);
-	console.log(CoinList['btc']);
+const SlackController = Botkit.slackbot({debug: true});
 
-	rtm.on(SlackClient.RTM_EVENTS.MESSAGE, async (event) => {
-		if (utils.isMessage(event) && utils.messageContainsText(event, opt.triggerOnWords)) {
-			let text = '';	
-			const searchedCoin = event.text.split(' ')[1].toUpperCase();
-			if (!CoinList[searchedCoin]) {
-				web.chat.postMessage(event.channel, 'Could not find coin ' + searchedCoin);
-			} else {
-				const res = await fetch('https://coinmarketcap-api.herokuapp.com/coins/' + CoinList[searchedCoin].id);
-				const coinData = await res.json();
-				const msgOptions = {
-					as_user: true,
-					attachments: [],
-				};
-				web.chat.postMessage(event.channel, JSON.stringify(coinData), msgOptions);
-				opt.logger.info(`Posting message to ${event.channel}`, msgOptions);
-			}
+const cryptoBot = function(botToken, CoinList) { 
+	const bot = SlackController.spawn({token: botToken}).startRTM();
+	SlackController.hears(
+		"\\?cm (.*)",
+		["direct_message", "direct_mention", "ambient"], async (bot, message) => {
+		const searchedCoin = message.text.split(' ')[1].toUpperCase();
+		if (!CoinList[searchedCoin]) {
+			bot.reply(message, 'Could not find coin ' + searchedCoin);
+		} else {
+			const res = await fetch('https://coinmarketcap-api.herokuapp.com/coins/' + CoinList[searchedCoin].id);
+			const coinData = await res.json();
+			bot.reply(message, JSON.stringify(coinData));
 		}
 	});
-
-	rtm.on(SlackClient.CLIENT_EVENTS.RTM.AUTHENTICATED, onAuthenticated);
-
-	rtm.start();
 };
 
 module.exports = cryptoBot;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cryptobot = require('./cryptobot');
 const utils = require('./utils');
-
+const fetch = require('node-fetch');
 if (!process.env.CRYPTOBOT_TOKEN) {
   console.error('You must setup the CRYPTOBOT_TOKEN environment variable before running the bot');
   process.exit(1);
@@ -9,6 +9,7 @@ if (!process.env.CRYPTOBOT_TOKEN) {
 // Because callbacks are so 2008 and .then are so 2015
 async function init() {
   const CoinList = await utils.getCoinList();
+  const res = await fetch('https://coinmarketcap-api.herokuapp.com/coins/bitcoin');
   cryptobot(process.env.CRYPTOBOT_TOKEN, CoinList);
 };
 
