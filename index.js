be6b8378b230a46f15e8ae1d6dde468bd3e0a3cc
Anto <anto@MacBook-Pro-de-Anto.local>
2018-01-06 12:59:34 +0100

    Binding heroku ports to prevent shutdown

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ dotenv.config();
 import fetch from "node-fetch";
 import { getCoinList } from "./utils";
 import slackBot from "./interfaces/slack";
+import http from "http";
+
+const server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.end();
+});
+
+server.listen(process.env.PORT || 80);
 
 if (!process.env.CRYPTOBOT_SLACK_TOKEN) {
   console.error(
