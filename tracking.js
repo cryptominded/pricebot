5813c9912be38f54ee70d2a2e9b11ac63446533e
anton <antoninribeaud@gmail.com>
2018-03-17 19:51:22 +0100

    New event tracking

--- tracking.js
+++ tracking.js
@@ -1,6 +1,22 @@
 import ua from 'universal-analytics';
+import Amplitude from 'amplitude';
+
+const amplitude = new Amplitude(process.env.AMPLITUDE);
 
 export default (teamId, eventData) => {
+  // Google Analytics
   const visitor = ua(process.env.GA, teamId, {https: true, strictCidFormat: false});
   visitor.event(eventData).send();
+
+  var data = {
+    event_type: eventData.ec, 
+    user_id: eventData.user, 
+    event_properties: {
+      coin: eventData.ea
+    },
+    user_properties: {
+      teamId,
+    }
+  };
+  amplitude.track(data);
 };
